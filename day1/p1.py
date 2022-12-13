1cfe71109c13a87d70df69f4de25af2fe95ead24
Rasmus Dan Petersen <Rasmus256@gmail.com>
2022-12-13 20:04:51 +0100

    first part solved

--- day1/p1.py
+++ day1/p1.py
@@ -1,13 +1,13 @@
 file1 = open('puzzle_input.csv', 'r')
 
 Lines = file1.readlines()
+counters = []
 count = 0
-currentMax = 0
 for line in Lines:
     if line != "\n":
         count += int(line.strip())
     else:
-        currentMax = max(currentMax, count)
+        counters.append(count)
         count=0
-print(currentMax)
 
+print("Max value element : ", max(counters))
