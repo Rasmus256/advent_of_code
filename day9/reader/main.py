4131c32a767c03667f84016d24cc6b8f1728c0ea
Rasmus Dan Petersen <Rasmus256@gmail.com>
2023-01-03 21:22:09 +0100

    updates image versions

---
+++ day9/reader/main.py
@@ -0,0 +1,24 @@
+import time
+import sys
+import re
+
+import stomp
+
+topic = "adventofcode.day9"
+
+hosts = [('amq.adventofcode.svc.cluster.local', 61613)]
+
+conn = stomp.Connection(host_and_ports=hosts)
+conn.connect('admin', 'admin', wait=True,headers = {'client-id': topic} )
+
+file1 = open('puzzle_input.csv', 'r')
+
+Lines = file1.readlines()
+for line in Lines:
+    line = line.strip()
+    parts = line.split(' ')
+    for x in range(int(parts[1])):
+      conn.send(body=f"{parts[0]}" , destination=topic)
+conn.send(body="EOM", destination=topic)
+
+conn.disconnect()
