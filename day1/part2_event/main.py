ad581398431f726c39e5b632a2db5a217d59b32b
Rasmus Dan Petersen <Rasmus256@gmail.com>
2023-01-03 20:21:58 +0100

    alters pipeline

--- day1/part2_event/main.py
+++ day1/part2_event/main.py
@@ -24,19 +24,8 @@ hosts = [('amq.adventofcode.svc.cluster.local', 61613)]
 conn = stomp.Connection(host_and_ports=hosts)
 conn.set_listener('', MyListener())
 conn.connect('admin', 'admin', wait=True,headers = {'client-id': 'day2_part2'} )
-conn.subscribe(destination='adventofcode.day1.part2', id=2, ack='auto',headers = {'subscription-type': 'MULTICAST','durable-subscription-name':'someValue'})
+conn.subscribe(destination='adventofcode.day1', id=2, ack='auto',headers = {'subscription-type': 'MULTICAST','durable-subscription-name':'someValue'})
 
-file1 = open('puzzle_input.csv', 'r')
-
-Lines = file1.readlines()
-count = 0
-for line in Lines:
-    if line != "\n":
-        count += int(line.strip())
-    else:
-        conn.send(body=str(count), destination='adventofcode.day1.part2')
-        count=0
-conn.send(body="EOM", destination='adventofcode.day1.part2')
 while not EOMRev: 
     print("Waiting for EOM")
     time.sleep(1)
