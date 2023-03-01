423ee6dc99dc9501e7f90701efe07cca48519bd4
Rasmus Dan Petersen <rasmus256@gmail.com>
2023-03-01 14:56:21 +0100

    Update main.py

--- day24/part1/main.py
+++ day24/part1/main.py
@@ -1,9 +1,3 @@
-def calculateSum(name, translations):
-    sum = 0
-    for idx, l in enumerate(name[::-1]):
-      sum = sum + translations[l]*pow(10, idx)
-    return sum
-
 firstinput = "send"
 secondinput = "more"
 thirdinput = "money"
@@ -12,24 +6,31 @@ print(firstinput)
 print(secondinput)
 print(thirdinput)
 
+def calculateSum(name, translations):
+    sum = 0
+    for idx, l in enumerate(name[::-1]):
+      sum = sum + translations[l]*pow(10, idx)
+    return sum
+def determineWhetherMatch(translations):
+    global firstinput, secondinput, thirdinput
+    return calculateSum(firstinput, translations) + calculateSum(secondinput, translations) == calculateSum(thirdinput, translations)
+def iterateoverLetters(letters, translations, level):
+    if len(letters) == 0:
+        if determineWhetherMatch(translations):
+            print(translations)
+            print(f"{level}, {translations}")
+    if len(letters) >0:
+        for i in range(10):
+            tr = translations.copy()
+            tr[letters[0]] = i
+            iterateoverLetters(letters[1::], tr, level+1)
 
 letters = list(set(''.join(firstinput).join(secondinput).join(thirdinput)))
 letters.sort()
 
 print(letters)
+translations = {}
+for i in letters:
+    translations[i] = 0
 
-
-def iterateoverLetters(letters, translations):
-    global firstinput
-    global secondinput
-    global thirdinput
-    if len(letters) == 0:
-        if calculateSum(firstinput, translations) + calculateSum(secondinput, translations) == calculateSum(thirdinput, translations):
-        	print(translations)
-    if len(letters) >0:
-        for i in range(10):
-            tr = translations.copy()
-            tr[letters[0]] = i
-            iterateoverLetters(letters[1::], tr)
-
-iterateoverLetters(firstinput, {})
+iterateoverLetters(letters, translations, 0)
