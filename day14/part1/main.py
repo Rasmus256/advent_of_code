95b052d4cfd1687a2d8dc8b29b404ecb90ffb079
Rasmus Dan Petersen <rasmus256@gmail.com>
2023-03-01 20:30:39 +0100

    Update main.py

--- day14/part1/main.py
+++ day14/part1/main.py
@@ -32,7 +32,7 @@ for line in Lines:
         startcoords = segments[0].split(",")
         msg = {'x': int(startcoords[0]),'y': int(startcoords[1])}
         conn.send(body=json.dumps(msg) , destination=topic)
-    for i in range(1,len(segments)+1):
+    for i in range(1,len(segments)):
         startcoords = segments[i-1].split(",")
         endcoords   = segments[i].split(",")
         if startcoords[0] == endcoords[0] : #vertical line
