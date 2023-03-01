144fccd3f9903e506534a1c2f07da1be5ec2e50a
Rasmus Dan Petersen <rasmus256@gmail.com>
2023-03-01 10:05:22 +0100

    Update main.py

--- day24/part1/main.py
+++ day24/part1/main.py
@@ -2,13 +2,14 @@ for s in range(10):
     for e in range(10):
         for n in range(10):
             for d in range(10):
-                for m in range(10):
-                    for o in range(10):
+                for f in range(10):
+                    for l in range(10):
                         for r in range(10):
-                            for y in range(10):
-                                first = s*1000+e*100+n*10+d
-                                second = m*1000+o*100+r*10+e
-                                third = m*10000+o*1000+n*100+e*10+y
-                                
-                                if first+second == third:
-                                    print(f"{s} {e} {n} {d} {m} {o} {r} {y}")
+                            for p in range(10):
+                                for g in range(10):
+                                    first = s*1000+e*100+n*10+d
+                                    second = f*1000+l*100+e*10+r
+                                    third = p*10000+e*1000+n*100+g*10+e
+
+                                    if first+second == third:
+                                        print(f"{s} {e} {n} {d} {f} {l} {r} {p} {g}")
