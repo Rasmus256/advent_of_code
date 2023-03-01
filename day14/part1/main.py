e537787a6ee00fd1dd3134e552c2f0b4902290a9
Rasmus Dan Petersen <rasmus256@gmail.com>
2023-03-01 20:44:08 +0100

    type

--- day14/part1/main.py
+++ day14/part1/main.py
@@ -49,7 +49,7 @@ for line in Lines:
             print(f"msg {msg}")
             conn.send(body=json.dumps(msg) , destination=topic)
         else: #horizontal line
-            for x in rrange(min(int(startcoords[0]), int(endcoords[0])),max(int(startcoords[0]), int(endcoords[0]) )) :
+            for x in range(min(int(startcoords[0]), int(endcoords[0])),max(int(startcoords[0]), int(endcoords[0]) )) :
                 msg = {'x': x,'y': int(startcoords[1])}
                 print(f"msg {msg}")
                 conn.send(body=json.dumps(msg) , destination=topic)
