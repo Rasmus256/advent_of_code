9263b8c0cc85afde453cd1e0f94aae5dd552234f
Rasmus Dan Petersen <rasmus256@gmail.com>
2023-03-01 10:52:16 +0100

    Update main.py

--- day24/part1/main.py
+++ day24/part1/main.py
@@ -1,49 +1,35 @@
-import os
-translations = {}
-
-def calculateSum(name):
-    global translations
+def calculateSum(name, translations):
     sum = 0
     for idx, l in enumerate(name[::-1]):
       sum = sum + translations[l]*pow(10, idx)
     return sum
 
 firstinput = "send"
-secondinput = "world"
-thirdinput = "hi"
+secondinput = "more"
+thirdinput = "money"
 
-letters = set(''.join(firstinput).join(secondinput).join(thirdinput))
+print(firstinput)
+print(secondinput)
+print(thirdinput)
+
+
+letters = list(set(''.join(firstinput).join(secondinput).join(thirdinput)))
+letters.sort()
 
 print(letters)
 
-for i in letters:
-    translations[i] = None
 
 def iterateoverLetters(letters, translations):
     global firstinput
+    global secondinput
+    global thirdinput
     if len(letters) == 0:
-        print(calculateSum(firstinput))
+        if calculateSum(firstinput, translations) + calculateSum(secondinput, translations) == calculateSum(thirdinput, translations):
+        	print(translations)
     if len(letters) >0:
         for i in range(10):
             tr = translations.copy()
             tr[letters[0]] = i
             iterateoverLetters(letters[1::], tr)
 
-iterateoverLetters(firstinput, translations)
-    
-print("-----")
-for s in range(10):
-    for e in range(10):
-        for n in range(10):
-            for d in range(10):
-                for f in range(10):
-                    for l in range(10):
-                        for r in range(10):
-                            for p in range(10):
-                                for g in range(10):
-                                    first = s*1000+e*100+n*10+d
-                                    second = f*1000+l*100+e*10+r
-                                    third = p*10000+e*1000+n*100+g*10+e
-
-                                    if first+second == third:
-                                        print(f"{s} {e} {n} {d} {f} {l} {r} {p} {g}")
+iterateoverLetters(firstinput, {})
