40597c1c3d39bddd3cf3ad7183eef245934110b7
Rasmus256 <rasmus256@gmail.com>
2023-03-10 16:30:32 +0100

    dict python syntax

--- day14/part1/main.py
+++ day14/part1/main.py
@@ -15,9 +15,9 @@ class MyListener(stomp.ConnectionListener):
             EOMRev = True
         else:
             rock = json.loads(message.body)
-            if not rock.x in rocks:
-                rocks[rock.x] = []
-            rocks[rock.x].append(int(rock.y))
+            if not rock['x'] in rocks:
+                rocks[rock['x']] = []
+            rocks[rock['x']].append(int(rock.y))
 
 hosts = [('amq.default.svc.cluster.local', 61613)]
 
