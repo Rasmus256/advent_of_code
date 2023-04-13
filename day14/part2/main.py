656ef9489eeddc81755753224f42acdc91840f51
Rasmus256 <rasmus256@gmail.com>
2023-04-13 19:13:42 +0200

    Update main.py

--- day14/part2/main.py
+++ day14/part2/main.py
@@ -118,8 +118,8 @@ hosts = [('amq-hdls-svc.adventofcode.svc.cluster.local', 61613)]
 
 conn = stomp.Connection(host_and_ports=hosts)
 conn.set_listener('', MyListener()) 
-conn.connect('admin', 'admin', wait=True,headers = {'client-id': topic} )
-conn.subscribe(destination=topic, id=131, ack='auto',headers = {'subscription-type': 'MULTICAST','durable-subscription-name':'someValue'})
+conn.connect(username,password, wait=True,headers = {'client-id': topic} )
+conn.subscribe(destination=topic+'::'+topic, id=142, ack='auto',headers = {'subscription-type': 'MULTICAST','durable-subscription-name':'someValue'})
 file1 = open('puzzle_input.csv', 'r')
 
 Lines = file1.readlines()
